4bbd5babeef7e7866187e2c4386efeb89a3dea85
yajun.qiao <yajun.qiao@bianlifeng.com>
2018-07-06 22:32:03 +0800

    测试目前支持的语法

--- app/route/Home.js
+++ app/route/Home.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-
+import { observer } from 'mobx-react';
+import store from './store';
+@observer
 export default class Home extends Component {
+  handleClick = () => {
+    store.handleChangeName();
+  }
+
+  handleTest = () => {
+    store.test();
+  }
+
+  handleAdd = () => {
+    // store.add();
+  }
   render() {
     return (
       <div>
         <div>
           <h2>welcome to home</h2>
+          <div>{store.name}</div>
+          <button onClick={this.handleClick}>async</button>
+          <button onClick={this.handleTest}>test await</button>
+          <button onClick={this.handleAdd}>add</button>
         </div>
       </div>
     );
