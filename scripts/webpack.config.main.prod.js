2bad5ecb973ccfe30c166295b6f47841df64b16b
yajun.qiao <yajun.qiao@bianlifeng.com>
2018-07-09 15:22:55 +0800

    添加打包主进程配置 && 测试打包主进程

--- scripts/webpack.config.main.prod.js
+++ scripts/webpack.config.main.prod.js
@@ -18,11 +18,11 @@ export default merge.smart(baseConfig, {
 
   target: 'electron-main',
 
-  entry: '../main',
+  entry: './main.js',
 
   output: {
     path: __dirname,
-    filename: '../dist/main.prod.js'
+    filename: './../dist/main.prod.js'
   },
 
   plugins: [
@@ -37,26 +37,12 @@ export default merge.smart(baseConfig, {
       openAnalyzer: process.env.OPEN_ANALYZER === 'true'
     }),
 
-    /**
-     * Create global constants which can be configured at compile time.
-     *
-     * Useful for allowing different behaviour between development builds and
-     * release builds
-     *
-     * NODE_ENV should be production so that modules do not perform certain
-     * development checks
-     */
     new webpack.EnvironmentPlugin({
       NODE_ENV: 'production',
       DEBUG_PROD: 'false'
     })
   ],
 
-  /**
-   * Disables webpack processing of __dirname and __filename.
-   * If you run the bundle in node.js it falls back to these values of node.js.
-   * https://github.com/webpack/webpack/issues/2010
-   */
   node: {
     __dirname: false,
     __filename: false
