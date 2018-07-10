92f38dff25d186d3fdc941863377fff1655cadb1
yajun.qiao <yajun.qiao@bianlifeng.com>
2018-07-10 16:47:43 +0800

    Change title

--- menu.js
+++ menu.js
@@ -45,14 +45,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About WCS',
           selector: 'orderFrontStandardAboutPanel:'
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide WCS',
           accelerator: 'Command+H',
           selector: 'hide:'
         },
